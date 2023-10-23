b551b7312b8f5d22229cbae40e715e2f3bb50544
Arcanjo <amanda.arcanjo@parceiro.mrv.com.br>
2023-10-23 16:03:12 -0300

    instalando dependencia react router dom

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Editando pra testar repositório parte 2.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Home-header">
+      <Link to="/lobby">
+        <p className="Button"> get started </p>
+      </Link>
     </div>
   );
 }
 
-export default App;
+export default Home;
